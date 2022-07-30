3ecbd056f49d21cf9138e63b29881a1a3ea4ce44
zakariamagdyz <zakariamagdy@ymail.com>
2022-07-30 18:05:48 +0200

    Initiate Dashboard with @MUI

---
+++ src/theme/theme.ts
@@ -0,0 +1,27 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+import { grey } from "@mui/material/colors";
+
+const getTheme = (darkMode: "ON" | "OFF") => {
+  let theme = createTheme({
+    palette: {
+      secondary: {
+        main: grey[600],
+      },
+      mode: darkMode === "ON" ? "dark" : "light",
+    },
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 450,
+        md: 769,
+        lg: 1027,
+        xl: 1400,
+      },
+    },
+  });
+  // add responsive to headers elements
+  theme = responsiveFontSizes(theme);
+  return theme;
+};
+
+export default getTheme;
